9ded3e92c94fd8f205623bdbe68564dda2da2d85
Rajat Mahajan <rajat.m.mahajan@oracle.com>
2021-07-26 20:42:17 +0100

    8270116: Expand ButtonGroupLayoutTraversalTest.java to run in all LaFs, including Aqua on macOS Summary: Expaneded ButtonGroupLayoutTraversalTest.java to run in all LAFs on all OS.

--- test/jdk/java/awt/Focus/FocusTraversalPolicy/ButtonGroupLayoutTraversal/ButtonGroupLayoutTraversalTest.java
+++ test/jdk/java/awt/Focus/FocusTraversalPolicy/ButtonGroupLayoutTraversal/ButtonGroupLayoutTraversalTest.java
@@ -37,6 +37,7 @@ import java.awt.Robot;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
 import java.awt.event.KeyEvent;
+import java.util.Arrays;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
@@ -55,93 +56,104 @@ public class ButtonGroupLayoutTraversalTest {
 
     static int focusCnt[] = new int[nx * ny];
     private static JFrame window;
+    private static Robot robot;
 
     public static void main(String[] args) throws Exception {
+        robot = new Robot();
+        robot.setAutoDelay(100);
 
-        try {
-            SwingUtilities.invokeAndWait(() -> changeLAF());
-            SwingUtilities.invokeAndWait(() -> initLayout(nx, ny));
-            Robot robot = new Robot();
-            robot.setAutoDelay(100);
-            robot.waitForIdle();
-            robot.delay(1000);
-
-            for (int i = 0; i < nx * ny - nx * ny / 2 - 1; i++) {
-                robot.keyPress(KeyEvent.VK_RIGHT);
-                robot.keyRelease(KeyEvent.VK_RIGHT);
-                robot.waitForIdle();
-            }
-
-            for (int i = 0; i < nx * ny / 2; i++) {
-                robot.keyPress(KeyEvent.VK_TAB);
-                robot.keyRelease(KeyEvent.VK_TAB);
-                robot.waitForIdle();
-            }
-
-            robot.delay(200);
-
-            for (int i = 0; i < nx * ny; i++) {
-                if (focusCnt[i] < 1) {
-                    throw new RuntimeException("Component " + i
-                        + " is not reachable in the forward focus cycle");
-                } else if (focusCnt[i] > 1) {
-                    throw new RuntimeException("Component " + i
-                        + " got focus more than once in the forward focus cycle");
+        for (UIManager.LookAndFeelInfo laf : UIManager.getInstalledLookAndFeels()) {
+            try {
+                if ("Nimbus".equals(laf.getName())) {
+                    continue;
                 }
+                SwingUtilities.invokeAndWait(() -> setLookAndFeel(laf));
+                SwingUtilities.invokeAndWait(() -> initLayout(nx, ny));
+                test();
+            } finally {
+                SwingUtilities.invokeAndWait(() -> {
+                    if (window != null) {
+                        window.dispose();
+                    }
+                    window = null;
+                    Arrays.fill(focusCnt, 0);
+                });
             }
+        }
+    }
 
-            for (int i = 0; i < nx * ny / 2; i++) {
-                robot.keyPress(KeyEvent.VK_SHIFT);
-                robot.keyPress(KeyEvent.VK_TAB);
-                robot.keyRelease(KeyEvent.VK_TAB);
-                robot.keyRelease(KeyEvent.VK_SHIFT);
-                robot.waitForIdle();
+
+    private static void test() {
+        robot.waitForIdle();
+        robot.delay(1000);
+
+        for (int i = 0; i < nx * ny - nx * ny / 2 - 1; i++) {
+            robot.keyPress(KeyEvent.VK_RIGHT);
+            robot.keyRelease(KeyEvent.VK_RIGHT);
+            robot.waitForIdle();
+        }
+
+        for (int i = 0; i < nx * ny / 2; i++) {
+            robot.keyPress(KeyEvent.VK_TAB);
+            robot.keyRelease(KeyEvent.VK_TAB);
+            robot.waitForIdle();
+        }
+
+        robot.delay(200);
+
+        for (int i = 0; i < nx * ny; i++) {
+            if (focusCnt[i] < 1) {
+                throw new RuntimeException("Component " + i
+                        + " is not reachable in the forward focus cycle");
+            } else if (focusCnt[i] > 1) {
+                throw new RuntimeException("Component " + i
+                        + " got focus more than once in the forward focus cycle");
             }
+        }
 
-            for (int i = 0; i < nx * ny - nx * ny / 2 - 1; i++) {
-                robot.keyPress(KeyEvent.VK_LEFT);
-                robot.keyRelease(KeyEvent.VK_LEFT);
-                robot.waitForIdle();
-            }
-
+        for (int i = 0; i < nx * ny / 2; i++) {
             robot.keyPress(KeyEvent.VK_SHIFT);
             robot.keyPress(KeyEvent.VK_TAB);
             robot.keyRelease(KeyEvent.VK_TAB);
             robot.keyRelease(KeyEvent.VK_SHIFT);
             robot.waitForIdle();
-
-            robot.delay(200);
-
-            for (int i = 0; i < nx * ny; i++) {
-                if (focusCnt[i] < 2) {
-                    throw new RuntimeException("Component " + i
-                        + " is not reachable in the backward focus cycle");
-                } else if (focusCnt[i] > 2) {
-                    throw new RuntimeException("Component " + i
-                        + " got focus more than once in the backward focus cycle");
-                }
-            }
-        } finally {
-            SwingUtilities.invokeAndWait(() -> {
-                if (window != null) {
-                    window.dispose();
-                }
-            });
         }
+
+        for (int i = 0; i < nx * ny - nx * ny / 2 - 1; i++) {
+            robot.keyPress(KeyEvent.VK_LEFT);
+            robot.keyRelease(KeyEvent.VK_LEFT);
+            robot.waitForIdle();
+        }
+
+        robot.keyPress(KeyEvent.VK_SHIFT);
+        robot.keyPress(KeyEvent.VK_TAB);
+        robot.keyRelease(KeyEvent.VK_TAB);
+        robot.keyRelease(KeyEvent.VK_SHIFT);
+        robot.waitForIdle();
+
+        robot.delay(200);
+
+        for (int i = 0; i < nx * ny; i++) {
+            if (focusCnt[i] < 2) {
+                throw new RuntimeException("Component " + i
+                        + " is not reachable in the backward focus cycle");
+            } else if (focusCnt[i] > 2) {
+                throw new RuntimeException("Component " + i
+                        + " got focus more than once in the backward focus cycle");
+            }
+        }
+
     }
 
-    private static void changeLAF() {
-        String currentLAF = UIManager.getLookAndFeel().toString();
-        currentLAF = currentLAF.toLowerCase();
-        if (currentLAF.contains("aqua") || currentLAF.contains("nimbus")) {
-            try {
-                UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
-            } catch (ClassNotFoundException
-                    | IllegalAccessException
-                    | InstantiationException
-                    | UnsupportedLookAndFeelException ex) {
-                ex.printStackTrace();
-            }
+    private static void setLookAndFeel(UIManager.LookAndFeelInfo laf) {
+        try {
+            UIManager.setLookAndFeel(laf.getClassName());
+            System.out.println(laf.getName());
+        } catch (UnsupportedLookAndFeelException ignored){
+            System.out.println("Unsupported LookAndFeel: " + laf.getClassName());
+        } catch (ClassNotFoundException | InstantiationException |
+                         IllegalAccessException e) {
+            throw new RuntimeException(e);
         }
     }
 
