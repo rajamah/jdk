a15e6d0c34f9b61d655f94fd20e4bfcfd0bd7bce
Alexey Ivanov <alexey.ivanov@oracle.com>
2022-08-25 09:10:42 +0100

    Put 2.75 at its place
    There was 2.25 instead

--- test/jdk/javax/swing/border/LineBorder/ScaledTextFieldBorderTest.java
+++ test/jdk/javax/swing/border/LineBorder/ScaledTextFieldBorderTest.java
@@ -47,7 +47,7 @@ public class ScaledTextFieldBorderTest {
 
     private static final double[] scales = {
             1.00, 1.25, 1.50, 1.75,
-            2.00, 2.25, 2.50, 2.25,
+            2.00, 2.25, 2.50, 2.75,
             3.00
     };
 
